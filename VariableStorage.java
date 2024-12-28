39a488629f1d4d10204c0c4ad774d04e75923e98
Temo <temodanelia19@gmail.com>
2024-12-28 21:29:45 +0400

    implemented variable storage for storing variables and getting the values

---
+++ VariableStorage.java
@@ -0,0 +1,21 @@
+import javax.naming.Name;
+import java.util.HashMap;
+import java.util.Map;
+
+public class VariableStorage {
+
+    public class VariableEnvironment {
+        private Map<String, Object> variables = new HashMap<>();
+
+        public void PutVariable(String name, Object value) {
+            variables.put(name,value);
+        }
+
+        public Object getVariable(String name) {
+           if (!variables.containsKey(name)){
+               throw new RuntimeException("Variable not defined " + name);
+           }
+           return variables.get(name);
+        }
+    }
+}
