90f2c9510da35c00129ff0de1f066e8866460e7c
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2024-12-29 17:43:53 +0400

    Fixed code style by removing unecessary nested class and added short descriptive comments

--- interpretercomponents/VariableStorage.java
+++ interpretercomponents/VariableStorage.java
@@ -4,19 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class VariableStorage {
+    // create a map where variables and their corresponding values will be stored
+    private final Map<String, Object> variables = new HashMap<>();
 
-    public class VariableEnvironment {
-        private Map<String, Object> variables = new HashMap<>();
+    // define a method which will be used to put variables into a variables map
+    public void PutVariable(String name, Object value) {
+        variables.put(name, value);
+    }
 
-        public void PutVariable(String name, Object value) {
-            variables.put(name,value);
+    // define a getter method which will be used to get the value of a corresponding variable
+    public Object getVariable(String name) {
+        if (!variables.containsKey(name)){
+            throw new RuntimeException("Variable not defined " + name);
         }
 
-        public Object getVariable(String name) {
-           if (!variables.containsKey(name)){
-               throw new RuntimeException("Variable not defined " + name);
-           }
-           return variables.get(name);
-        }
+        return variables.get(name);
     }
 }
