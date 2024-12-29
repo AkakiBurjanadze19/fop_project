a1a2197daecc4f1ca8eb170c4dd54e1046eb11cc
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2024-12-29 17:56:55 +0400

    remove unnecessary nested class from VariableStorage and add short descriptive comments

--- VariableStorage.java
+++ VariableStorage.java
@@ -1,21 +1,21 @@
-import javax.naming.Name;
 import java.util.HashMap;
 import java.util.Map;
 
 public class VariableStorage {
+    // create a variables map where variables and their corresponding values will be stored
+    private final Map<String, Object> variables = new HashMap<>();
 
-    public class VariableEnvironment {
-        private Map<String, Object> variables = new HashMap<>();
+    // define a method which will be used to put variables into a variables map
+    public void PutVariable(String name, Object value) {
+        variables.put(name,value);
+    }
 
-        public void PutVariable(String name, Object value) {
-            variables.put(name,value);
+    // define a getter method for getting variable values from variables map
+    public Object getVariable(String name) {
+        if (!variables.containsKey(name)) {
+            throw new RuntimeException("Variable not defined " + name);
         }
 
-        public Object getVariable(String name) {
-           if (!variables.containsKey(name)){
-               throw new RuntimeException("Variable not defined " + name);
-           }
-           return variables.get(name);
-        }
+        return variables.get(name);
     }
 }
