a5ca137a101b1d60c52fb05f06ca193a1cf6719c
Temo <temodanelia19@gmail.com>
2024-12-29 21:55:13 +0400

    added tests for the VariableStorage data structure

--- componenttests/VariablStorageTest.java
+++ componenttests/VariablStorageTest.java
@@ -1,2 +1,14 @@
-package componenttests;public class VariablStorageTest {
+package componenttests;
+import interpretercomponents.VariableStorage;
+public class VariablStorageTest {
+    public static void main(String[] args){
+
+        VariableStorage variables = new VariableStorage();
+        variables.PutVariable("user", "abc");
+        variables.PutVariable("user","ilia");
+        String name = (String) variables.getVariable("user");
+        System.out.println("name: " + name);
+
+
+    }
 }
