862acb0d7ad56130a646819c8c745197ac075a61
Akaki <akakiburjanadze17@gmail.com>
2024-12-29 17:18:34 +0400

    Update Token.java

--- interpretercomponents/Token.java
+++ interpretercomponents/Token.java
@@ -1,27 +1,13 @@
-<<<<<<< HEAD:Token.java
-public class Token {
-    public static final String IDENTIFIER = "IDENTIFIER"; // for variable names
-    public static final String NUMBER = "NUMBER"; // for numeric literals
-    public static final String STRING = "STRING"; // for string literals
-    public static final String KEYWORD = "KEYWORD"; // for reserved keywords. for example: for, do, e.t.c
-    public static final String OPERATOR = "OPERATOR"; // for arithmetic operations:   (+,-,*,/)
-    public static final String DELIMITER = "DELIMITER"; // for symbols. for example: ";".
-    public static final String EOF = "EOF"; // end of input
-
-    private final String type; // type of the token
-    private final String value; // actual value of the token type
-=======
 package interpretercomponents;
 
 public class Token {
-        public static final String IDENTIFIER = "IDENTIFIER";
-        public static final String NUMBER = "NUMBER";
-        public static final String STRING = "STRING";
-        public static final String KEYWORD = "KEYWORD";
-        public static final String OPERATOR = "OPERATOR";
-        public static final String DELIMETER = "DELIMETER";
-        public static final String EOF = "EOF";
->>>>>>> origin/component-tests:interpretercomponents/Token.java
+    public static final String IDENTIFIER = "IDENTIFIER";
+    public static final String NUMBER = "NUMBER";
+    public static final String STRING = "STRING";
+    public static final String KEYWORD = "KEYWORD";
+    public static final String OPERATOR = "OPERATOR";
+    public static final String DELIMETER = "DELIMETER";
+    public static final String EOF = "EOF";
 
     // constructor which initializes token type and value
     public Token(String type, String value) {
@@ -39,21 +25,8 @@ public class Token {
         return this.value;
     }
 
-<<<<<<< HEAD:Token.java
     // method for printing the token structure (for debugging purposes)
     public String toString() {
         return "Token{" + "type='" + type + '\'' + ", value='" + value + '\'' + '}';
     }
-=======
-
-            public String getValue() {
-                return this.value;
-            }
-
-            public String toString() {
-                return "Token{" + "type='" + type + '\'' + ", value='" + value + '\'' + '}';
-            }
-
-
->>>>>>> origin/component-tests:interpretercomponents/Token.java
 }
