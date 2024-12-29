25145669eccf567c9096a568eeb5da15e6a1f200
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2024-12-29 18:42:47 +0400

    Fix code style

--- componenttests/TestComponents.java
+++ componenttests/TestComponents.java
@@ -1,18 +1,14 @@
 package componenttests;
 import interpretercomponents.Token;
+
 public class TestComponents {
     public static void main(String[] args){
-        System.out.println(new Token(Token.IDENTIFIER,"x"));
-        System.out.println(new Token(Token.NUMBER,"8"));
-        System.out.println(new Token(Token.STRING,"nigger"));
-        System.out.println(new Token(Token.KEYWORD,"if"));
-        System.out.println(new Token(Token.OPERATOR,"-"));
-        System.out.println(new Token(Token.DELIMETER,")"));
-        System.out.println(new Token(Token.EOF,""));
-
+        System.out.println(new Token(Token.IDENTIFIER, "x"));
+        System.out.println(new Token(Token.NUMBER, "8"));
+        System.out.println(new Token(Token.STRING, "nigger"));
+        System.out.println(new Token(Token.KEYWORD, "if"));
+        System.out.println(new Token(Token.OPERATOR, "-"));
+        System.out.println(new Token(Token.DELIMITER, ")"));
+        System.out.println(new Token(Token.EOF, ""));
     }
-
-
 }
-
-
