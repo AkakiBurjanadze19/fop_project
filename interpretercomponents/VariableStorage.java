82dd86d451a2ae3b6d362113f4951ec8cf2abe47
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2024-12-29 18:43:10 +0400

    Fix code style

--- interpretercomponents/VariableStorage.java
+++ interpretercomponents/VariableStorage.java
@@ -4,19 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class VariableStorage {
+    private final Map<String, Object> variables = new HashMap<>();
 
-    public class VariableEnvironment {
-        private Map<String, Object> variables = new HashMap<>();
+    public void PutVariable(String name, Object value) {
+        variables.put(name,value);
+    }
 
-        public void PutVariable(String name, Object value) {
-            variables.put(name,value);
+    public Object getVariable(String name) {
+        if (!variables.containsKey(name)){
+            throw new RuntimeException("Variable not defined " + name);
         }
 
-        public Object getVariable(String name) {
-           if (!variables.containsKey(name)){
-               throw new RuntimeException("Variable not defined " + name);
-           }
-           return variables.get(name);
-        }
+        return variables.get(name);
     }
 }
