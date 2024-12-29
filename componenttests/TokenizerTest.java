ce277fd0c25b7183b319e3ffee3036ae83021e75
Temo <temodanelia19@gmail.com>
2024-12-29 21:54:16 +0400

    added tests for the Tokenizer

--- componenttests/TokenizerTest.java
+++ componenttests/TokenizerTest.java
@@ -1,2 +1,16 @@
-package componenttests;public class TokenizerTest {
+package componenttests;
+import interpretercomponents.Tokenizer;
+import java.util.List;
+import interpretercomponents.Token;
+public class TokenizerTest {
+    public static void main (String[] args){
+
+        String input = "puts 10 + 20";
+        Tokenizer tokenizer = new Tokenizer(input);
+        List <Token> tokens = tokenizer.tokenize();
+        for (Token token : tokens){
+            System.out.println(token);
+        }
+
+    }
 }
