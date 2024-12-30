745c6b10e9ae1967fdb28576c26c99b1445bb33d
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2024-12-31 00:41:43 +0400

    tweaked code style

--- algorithmstests/ReverseNumber.java
+++ algorithmstests/ReverseNumber.java
@@ -2,8 +2,8 @@ package algorithmstests;
 import interpretercomponents.Interpreter;
 public class ReverseNumber {
     public static void main(String[] args){
-
         StringBuilder code = new StringBuilder();
+
         code.append("n = 12345\n");
         code.append("reversed = 0\n");
         code.append("while n != 0 {\n");
@@ -15,7 +15,5 @@ public class ReverseNumber {
 
         Interpreter interpreter = new Interpreter(code.toString());
         interpreter.execute();
-
-
     }
 }
