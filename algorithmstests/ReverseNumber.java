e91dc8c9de65c9e8ca8f5fe1044bd65892837cd1
Temo <temodanelia19@gmail.com>
2024-12-30 22:40:35 +0400

    added test for reverse number

--- algorithmstests/ReverseNumber.java
+++ algorithmstests/ReverseNumber.java
@@ -1,2 +1,21 @@
-package algorithmstests;public class ReverseNumber {
+package algorithmstests;
+import interpretercomponents.Interpreter;
+public class ReverseNumber {
+    public static void main(String[] args){
+
+        StringBuilder code = new StringBuilder();
+        code.append("n = 12345\n");
+        code.append("reversed = 0\n");
+        code.append("while n != 0 {\n");
+        code.append(" digit = n % 10\n");
+        code.append(" reversed = reversed * 10 + digit\n");
+        code.append(" n = n / 10\n");
+        code.append("}\n");
+        code.append("puts reversed\n");
+
+        Interpreter interpreter = new Interpreter(code.toString());
+        interpreter.execute();
+
+
+    }
 }
