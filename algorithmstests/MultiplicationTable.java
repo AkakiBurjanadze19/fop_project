fa1ed470acf7191de0c079363bce7783cadc2d21
Temo <temodanelia19@gmail.com>
2024-12-30 22:39:51 +0400

    added test for multiplication table algorithm

--- algorithmstests/MultiplicationTable.java
+++ algorithmstests/MultiplicationTable.java
@@ -1,2 +1,22 @@
-package algorithmstests;public class MultiplicationTable {
+package algorithmstests;
+import interpretercomponents.Interpreter;
+public class MultiplicationTable {
+
+
+    public static void main(String[] args){
+
+        StringBuilder code = new StringBuilder();
+        code.append("n = 5\n");
+        code.append("i = 1\n");
+
+        code.append("while i <= 10 {\n");
+        code.append(" result = n * i\n");
+        code.append(" puts result\n");
+        code.append(" i = i + 1\n");           
+        code.append("}\n");
+
+        String input = code.toString();
+        Interpreter interpreter = new Interpreter(input);
+        interpreter.execute();
+    }
 }
